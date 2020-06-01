9c923652b7dd7e6c7a2dea13f5041c5d99edd662
Kieran Brown <kieranb662@gmail.com>
2020-06-01 09:18:40 -0400

    Initial Commit

---
+++ MySchema/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MySchema
+//
+//  Created by Kieran Brown on 6/1/20.
+//  Copyright © 2020 Kieran Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
